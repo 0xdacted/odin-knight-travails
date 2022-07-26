48a957a9a5f9ee810d6f772cde0c14629ba3e04f
Red <crabmarket56@gmail.com>
2022-07-26 14:47:51 -0400

    Return root to create working tree

--- knight_travails.rb
+++ knight_travails.rb
@@ -4,7 +4,7 @@
 # Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
 
 class Knight
-  attr_accessor :row, :column
+  attr_accessor :row, :column, :up_right, :up_left, :right_up, :left_up, :down_right, :down_left, :right_down, :left_down
   def initialize(row = rand(0...7), column = rand(0...7))
     @row = row
     @column = column
@@ -22,28 +22,28 @@ end
 
 # create a tree of every possible move the knight can make from a given position on the board
 class Board
-  attr_accessor :grid
+  attr_accessor :grid, :root, :knight
   def initialize
    @grid =  Array.new(8) {Array.new(8)}
    @knight = Knight.new(3, 3)
-   move_knight(@knight.row, @knight.column, @grid)
-   
+   @root = move_knight(@knight.row, @knight.column, @grid)
+   p @root
   end
 
   def move_knight (row, column, grid)
     return if row > 7 || row < 0 || column > 7 || column < 0
-    knight = Knight.new(row, column)
-    grid[row][column] = knight
+    root = Knight.new(row, column)
+    grid[row][column] = root
     
     begin
-    knight.up_right = move_knight(row + 2, column + 1, grid) unless row > 5 || column > 6 || grid[row + 2][column + 1] != nil
-    knight.up_left = move_knight(row + 2, column - 1, grid) unless row > 5 || column < 1 || grid[row + 2][column - 1] != nil
-    knight.right_up = move_knight(row + 1, column + 2, grid) unless row > 6 || column > 5 || grid[row + 1][column + 2] != nil
-    knight.left_up = move_knight(row + 1, column - 2, grid) unless row > 6 || column < 2 || grid[row + 1][column - 2] != nil
-    knight.down_right = move_knight(row - 2, column + 1, grid) unless row < 2 || column > 6 || grid[row - 2][column + 1] != nil
-    knight.down_left = move_knight(row - 2, column - 1, grid) unless row < 2 || column < 1 || grid[row - 2][column - 1] != nil
-    knight.right_down = move_knight(row - 1, column + 2, grid) unless row < 1 || column > 5 || grid[row - 1][column + 2] != nil
-    knight.left_down = move_knight(row - 1, column - 2, grid) unless row < 1 || column < 2 || grid[row - 1][column - 2] != nil
+      root.up_right = move_knight(row + 2, column + 1, grid) unless row > 5 || column > 6 || grid[row + 2][column + 1] != nil
+      root.up_left = move_knight(row + 2, column - 1, grid) unless row > 5 || column < 1 || grid[row + 2][column - 1] != nil
+      root.right_up = move_knight(row + 1, column + 2, grid) unless row > 6 || column > 5 || grid[row + 1][column + 2] != nil
+      root.left_up = move_knight(row + 1, column - 2, grid) unless row > 6 || column < 2 || grid[row + 1][column - 2] != nil
+      root.down_right = move_knight(row - 2, column + 1, grid) unless row < 2 || column > 6 || grid[row - 2][column + 1] != nil
+      root.down_left = move_knight(row - 2, column - 1, grid) unless row < 2 || column < 1 || grid[row - 2][column - 1] != nil
+      root.right_down = move_knight(row - 1, column + 2, grid) unless row < 1 || column > 5 || grid[row - 1][column + 2] != nil
+      root.left_down = move_knight(row - 1, column - 2, grid) unless row < 1 || column < 2 || grid[row - 1][column - 2] != nil
     rescue
       grid.each_with_index do |row, row_index|
         row.each_with_index do |space, column_index|
@@ -53,11 +53,14 @@ class Board
         end
       end
     end
-    p grid
+    root
   end
+
+  
+
 end
 
 
-
-
 chess_board = Board.new
+
+
