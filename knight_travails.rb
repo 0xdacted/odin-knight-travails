eb337d6e406146dc5b3e0e3bbc09c64bfaca3a27
Red <crabmarket56@gmail.com>
2022-07-26 11:15:50 -0400

    Add current_location tracker

--- knight_travails.rb
+++ knight_travails.rb
@@ -2,15 +2,26 @@ class Board
   def initialize
    @grid =  Array.new(8) {Array.new(8)}
    @knight = "K"
+   place_knight
   end
 
-  def place_knight(column = rand(0...7), row = rand(0...7))
-    @grid[column][row] = @knight
+  def place_knight(row = rand(0...7), column = rand(0...7))
+    @grid[row][column] = @knight
     p @grid
+  end
+
+  def move_knight
+    @grid.each_with_index do |row, row_index|
+      row.each_with_index do |square, column_index| if square == "K"
+        current_location = [row_index],[column_index]
+        p current_location
+      end
+      end
+    end
   end
 end
 
 
 
 chess_board = Board.new
-chess_board.place_knight
+chess_board.move_knight
