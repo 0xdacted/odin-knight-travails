b5da0d02be078d873cf9441ee27e4411e12ad18e
Red <crabmarket56@gmail.com>
2022-07-26 11:49:18 -0400

    Restart with Knight class

--- knight_travails.rb
+++ knight_travails.rb
@@ -1,23 +1,26 @@
+#  Put together a script that creates a game board and a knight.
+# Treat all possible moves the knight could make as children in a tree. Don’t allow any moves to go off the board.
+# Decide which search algorithm is best to use for this case. Hint: one of them could be a potentially infinite series.
+# Use the chosen search algorithm to find the shortest path between the starting square (or node) and the ending square. Output what that full path looks like, e.g.:
+
+
+class Knight
+  def initialize(row = rand(0...7), column = rand(0...7))
+    @row = row
+    @column = column
+    @move_tree = build_tree(row, column)
+  end
+
+  def build_tree(row, column)
+    return if @row > 7 || @row < 0 || @column > 7 || @column < 0
+      
+
+  end
+end
+
 class Board
   def initialize
    @grid =  Array.new(8) {Array.new(8)}
-   @knight = "K"
-   place_knight
-  end
-
-  def place_knight(row = rand(0...7), column = rand(0...7))
-    @grid[row][column] = @knight
-    p @grid
-  end
-
-  def move_knight
-    @grid.each_with_index do |row, row_index|
-      row.each_with_index do |square, column_index| if square == "K"
-        current_location = [row_index],[column_index]
-        p current_location
-      end
-      end
-    end
   end
 end
 
